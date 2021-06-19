eb641029ed4585abd14d9d5f0f79ae78b91bb655
Varun Khalate <khalatevarun@gmail.com>
2021-06-19 13:45:46 +0530

    add date file to store constants

---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+import apple from '../icons/apple.png';
+import footprint from '../icons/footprint.png';
+import dumbell from '../icons/dumbell.png';
+import moon from '../icons/moon.png';
+
+export const dailyStats = [
+  {
+    img: { apple },
+    type: 'Calories',
+    stat: '2814',
+    status: 'Daily Average',
+  },
+  {
+    img: { footprint },
+    type: 'Steps',
+    stat: '7234',
+    status: 'Daily Average',
+  },
+  {
+    img: { dumbell },
+    type: 'Workouts',
+    stat: '1h 23min',
+    status: 'Active',
+  },
+  {
+    img: { moon },
+    type: 'Sleeping Analysis',
+    stat: '7h 35min',
+    status: 'In Bad Average',
+  },
+];
