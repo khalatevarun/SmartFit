885821ee7c04931d0ba8cdb2f51c7b2cccb5c8e8
Varun Khalate <khalatevarun@gmail.com>
2021-06-20 01:29:31 +0530

    complete sidebar section

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,61 @@
+import calendar from '../../icons/calendar.png';
+import './style.scss';
+
 const Menu = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="menu">
+      <div className="menu-title">Dashboard</div>
+      <div className="menu-link">
+        <img src={calendar} className="icon" />
+        <p>Overview</p>
+      </div>
+      <div className="menu-link">
+        <img src={calendar} className="icon" />
+        <p>Daily Plan</p>
+      </div>
+      <div className="menu-link">
+        <img src={calendar} className="icon" />
+        <p>Progress</p>
+      </div>
+      <div className="menu-title">Personal Tips</div>
+      <div className="menu-link">
+        <img src={calendar} className="icon" />
+        <p>Overview</p>
+      </div>
+      <div className="menu-link">
+        <img src={calendar} className="icon" />
+        <p>Overview</p>
+      </div>
+      <div className="menu-link">
+        <img src={calendar} className="icon" />
+        <p>Overview</p>
+      </div>
+      <div className="menu-link">
+        <img src={calendar} className="icon" />
+        <p>Daily Plan</p>
+      </div>
+      <div className="menu-link">
+        <img src={calendar} className="icon" />
+        <p>Progress</p>
+      </div>
+      <div className="menu-title">Your Data</div>
+      <div className="menu-link">
+        <img src={calendar} className="icon" />
+        <p>Overview</p>
+      </div>
+      <div className="menu-link">
+        <img src={calendar} className="icon" />
+        <p>Daily Plan</p>
+      </div>
+      <div className="menu-link">
+        <img src={calendar} className="icon" />
+        <p>Progress</p>
+      </div>
+      <div className="menu-link">
+        <img src={calendar} className="icon" />
+        <p>Overview</p>
+      </div>
+    </div>
+  );
 };
 export default Menu;
